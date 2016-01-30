b783a57cb4077889eb0d7f3750b169a3881a5b31
hveldstra <h@veldstra.org>
2016-01-30 16:54:26 +0000

    Use nconf for config

--- index.js
+++ index.js
@@ -2,28 +2,31 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var nconf = require('nconf');
 
 var app = express();
+
+nconf.argv().env().file({file: './config/default.json'});
 
 // Specify the connection string for your mongodb database
 // and the location to your Parse cloud code
 var api = new ParseServer({
-  databaseURI: 'mongodb://localhost:27017/dev',
-  //cloud: '/home/myApp/cloud/main.js', // Provide an absolute path
-  appId: 'myAppId',
-  masterKey: 'mySecretMasterKey',
-  fileKey: 'optionalFileKey'
+  databaseURI: nconf.get('databaseURI'),
+  //cloud: nconf.get('cloud'),
+  appId: nconf.get('appId'),
+  masterKey: nconf.get('masterKey'),
+  fileKey: nconf.get('fileKey')
 });
 
 // Serve the Parse API on the /parse URL prefix
-app.use('/parse', api);
+app.use(nconf.get('parseAPIPrefix'), api);
 
 // Hello world
 app.get('/', function(req, res) {
   res.status(200).send('Express is running here.');
 });
 
-var port = process.env.PORT || 1337;
+var port = nconf.get('port');
 app.listen(port, function() {
-  console.log('parse-server-example running on port ' + port + '.');
+  console.log('parse-server-example running on port %s', port);
 });
